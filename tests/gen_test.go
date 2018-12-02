f0c4745cb876ff946d29e1f9883aa57ffc6a50c2
jsd115 <jsd115@fb.com>
2018-12-02 09:38:56 -0800

    Fix mapping for protobuf string and bytes types
    In Python 3 and the protobuf 3.6.1, the `bytes` type maps to
    Python's `bytes` type while `string` maps to Python's `str`. This leads
    to type errors when using the generated stubs.

--- tests/gen_test.go
+++ tests/gen_test.go
@@ -24,7 +24,7 @@ message Foo {
 // these are written without newlines or spaces
 // while this makes the tests less comprehensive, it also makes them **far** less
 // brittle and horrible to write.
-var simpleMypy = "fromtypingimportcast,Dict,List,TupleclassFoo:bar:stringbaz:intdef__init__(self,bar:string=None,baz:int=None)->Foo:...defCopyFrom(self,other:Foo)->None:...@staticmethoddefListFields()->Tuple[FieldDescriptor,value]:..."
+var simpleMypy = "fromtypingimportcast,Dict,List,TupleclassFoo:bar:strbaz:intdef__init__(self,bar:str=None,baz:int=None)->Foo:...defCopyFrom(self,other:Foo)->None:...defSerializeToString(self)->str:...defParseFromString(self,data:str)->None:...@staticmethoddefListFields()->Tuple[FieldDescriptor,value]:..."
 
 var enumProto = `
 enum Foo {
@@ -34,7 +34,7 @@ enum Foo {
 }
 `
 
-var enumMypy = "fromtypingimportcast,Dict,List,TupleclassFoo:BAR:AnyBAZ:AnyQUUX:Anydef__init__(self,BAR:Any=None,BAZ:Any=None,QUUX:Any=None)->Foo:...defName(enumClass:Foo)->Any:...defValue(memberName:str)->Any:...defCopyFrom(self,other:Foo)->None:...@staticmethoddefListFields()->Tuple[FieldDescriptor,value]:..."
+var enumMypy = "fromtypingimportcast,Dict,List,TupleclassFoo:BAR:AnyBAZ:AnyQUUX:Anydef__init__(self,BAR:Any=None,BAZ:Any=None,QUUX:Any=None)->Foo:...defName(enumClass:Foo)->Any:...defValue(memberName:str)->Any:...defCopyFrom(self,other:Foo)->None:...defSerializeToString(self)->str:...defParseFromString(self,data:str)->None:...@staticmethoddefListFields()->Tuple[FieldDescriptor,value]:..."
 
 var tests = [][]string{
 	{simpleProto, simpleMypy},
